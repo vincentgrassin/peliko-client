f4ab22b9ebe16479d03317ede6ca2a9150ee5868
Loic <loic.bovo@protonmail.com>
2021-01-18 21:36:36 -0500

    change a bit the code and added tabview for the navigation

--- application/mobile/App.tsx
+++ application/mobile/App.tsx
@@ -1,21 +1,15 @@
 
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
-import { Home } from './src/screens/home'
-import { Notifications } from './src/screens/notifications'
-
-
-const stack = createStackNavigator();
+import {View} from 'react-native'
+import {MainTabView} from './src/navigation'
 
 export default function App() {
+  // TODO: add some footer
   return (
-    <NavigationContainer> 
-      <stack.Navigator>
-        <stack.Screen name='Home' component={Home} options={{title:'Vos pelliko'}} />
-        <stack.Screen name='Notifications' component={Notifications} options={{title:'notif pelli'}} />
-      </stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <MainTabView />
+    </View>
   );
 }
---
+++ application/mobile/src/screens/rolls/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+interface RollProps {
+
+}
+
+export const Rolls: React.FC<RollProps> = (props) => {
+
+    return (<View></View>);
+
+}
