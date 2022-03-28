cb1258dce9252f8c2c6d071f3afc326c1dec5099
Vincent Grassin <vgrassin@talentsoft.com>
2022-03-28 18:27:14 +0200

    remove uneeded env var

--- application/mobile/src/utils/helpers/cloudinaryHelper.ts
+++ application/mobile/src/utils/helpers/cloudinaryHelper.ts
@@ -1,8 +1,6 @@
-import {
-  CLOUDINARY_CLOUD_NAME,
-  CLOUDINARY_UPLOAD_PRESET,
-  CLOUDINARY_UPLOAD_URL
-} from "@env";
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from "@env";
+
+export const cloudinaryBaseUploadUrl = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
 
 export const getCloudinaryUrl = (publicId: string, quality = 90) => {
   return `https://res.cloudinary.com/${CLOUDINARY_CLOUD_NAME}/image/upload/q_${quality}/${publicId}.jpg`;
@@ -11,14 +9,14 @@ export const getCloudinaryUrl = (publicId: string, quality = 90) => {
 export const uploadToCloudinary = async (base64: string) => {
   const data = {
     file: `data:image/jpg;base64,${base64}`,
-    upload_preset: CLOUDINARY_UPLOAD_PRESET
+    upload_preset: CLOUDINARY_UPLOAD_PRESET,
   };
-  const response = await fetch(CLOUDINARY_UPLOAD_URL, {
+  const response = await fetch(cloudinaryBaseUploadUrl, {
     body: JSON.stringify(data),
     headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
     },
-    method: "POST"
+    method: "POST",
   });
   const jsonResponse = await response.json();
   return jsonResponse;
--- application/mobile/src/utils/types/env.d.ts
+++ application/mobile/src/utils/types/env.d.ts
@@ -1,6 +1,5 @@
 declare module "@env" {
   export const BASE_URL: string;
-  export const CLOUDINARY_UPLOAD_URL: string;
   export const CLOUDINARY_UPLOAD_PRESET: string;
   export const CLOUDINARY_CLOUD_NAME: string;
 }
